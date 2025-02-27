091e3daf98e7778c08d13fc76b25cbd15d6a2605
nayanbecker <nayanbecker@gmail.com>
2025-02-27 09:47:38 -0300

    first commit 🔥

---
+++ src/main/java/com/nayan/app/crud_courses_app/CrudCoursesAppApplication.java
@@ -0,0 +1,13 @@
+package com.nayan.app.crud_courses_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCoursesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCoursesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nayan/app/crud_courses_app/CrudCoursesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayan.app.crud_courses_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCoursesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
